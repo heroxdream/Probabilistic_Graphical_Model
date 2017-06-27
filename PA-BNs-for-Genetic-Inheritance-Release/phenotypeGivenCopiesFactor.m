dd4f47884c6b3bbd662f5bd56c5a261c1457f395
heroxdream <heroxdream@gmail.com>
2017-06-27 11:00:47 -0400

    phenotypeGivenCopiesFactor.m test passed

--- PA-BNs-for-Genetic-Inheritance-Release/phenotypeGivenCopiesFactor.m
+++ PA-BNs-for-Genetic-Inheritance-Release/phenotypeGivenCopiesFactor.m
@@ -59,9 +59,21 @@ phenotypeFactor = struct('var', [], 'card', [], 'val', []);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
 
 % Fill in phenotypeFactor.var.  This should be a 1-D row vector.
+phenotypeFactor.var = [phenotypeVar, geneCopyVarOne, geneCopyVarTwo];
+
 % Fill in phenotypeFactor.card.  This should be a 1-D row vector.
+phenotypeFactor.card = [2, numAlleles, numAlleles];
 
 phenotypeFactor.val = zeros(1, prod(phenotypeFactor.card));
 % Replace the zeros in phentoypeFactor.val with the correct values.
+for p2 = 1: numAlleles 
+    for p1 = 1: numAlleles 
+        genoType = allelesToGenotypes(p1, p2);
+        prob = alphaList(genoType);
+        phenotypeFactor = SetValueOfAssignment(phenotypeFactor, [1, p1, p2], prob);
+        phenotypeFactor = SetValueOfAssignment(phenotypeFactor, [2, p1, p2], 1 - prob);
+    end
+end
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
